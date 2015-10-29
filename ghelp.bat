bdfd51d8a5bf79e1562e474a19b176a7676274e1
Marcos Rocha <marcos.rocha@db1.com.br>
2015-10-29 15:17:55 -0200

    Added help command ghelp

---
+++ ghelp.bat
@@ -0,0 +1,24 @@
+@echo off
+echo gicosh - GIT Command Shortcuts for Windows and Lazy Developers
+echo.
+echo The shortcuts and commands under each one are:
+echo.
+echo ษออออออออหออออออออออออออออออออออออออออออออออออออป
+echo บShortcutบGIT Command                           บ
+echo ฬออออออออฮออออออออออออออออออออออออออออออออออออออน
+echo บga.bat  บgit add                               บ
+echo บgb.bat  บgit branch                            บ
+echo บgc.bat  บgit commit                            บ
+echo บgl.bat  บgit log                               บ
+echo บgm.bat  บgit merge                             บ
+echo บgo.bat  บgit checkout                          บ
+echo บgp.bat  บgit pull                              บ
+echo บgpsh.batบgit push                              บ
+echo บgr.bat  บgit reset HEAD                        บ
+echo บgrh.bat บgit reset HEAD --hard                 บ
+echo บgrm.bat บgit rm                                บ
+echo บgs.bat  บgit status                            บ
+echo บgsh.bat บgit stash                             บ
+echo บgsi.bat บgit submodule init                    บ
+echo บgsu.bat บgit submodule update --remote --rebaseบ
+echo ศออออออออสออออออออออออออออออออออออออออออออออออออผ
